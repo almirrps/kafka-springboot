1fb085af9cd94b906c407737a8bfdfdf956d1720
almirrps <almirrps@hotmail.com>
2023-01-25 17:05:35 -0300

    Consumer: Realizando mapeamento de mensagens “tipadas”

--- consumer/src/main/java/org/almirsantos/consumer/OrderConsumer.java
+++ consumer/src/main/java/org/almirsantos/consumer/OrderConsumer.java
@@ -1,6 +1,7 @@
 package org.almirsantos.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +9,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class OrderConsumer {
 
+    //Método que gera o log simples
+//    @KafkaListener(topics = "${order.topic}", groupId = "${spring.kafka.consumer.group-id}")
+//    public void consumer(String order) {
+//        log.info("Order" + order);
+//    }
+
+    //Método que utiliza de um ConsumerRecord para gerar o log
     @KafkaListener(topics = "${order.topic}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consumer(String order) {
-        log.info("Order" + order);
+    public void consumer(ConsumerRecord consumerRecord) {
+        log.info("key: " + consumerRecord.key());
+        log.info("Headers: " + consumerRecord.headers());
+        log.info("Partion: " + consumerRecord.partition());
+        log.info("Order: " + consumerRecord.value());
     }
 
 }
